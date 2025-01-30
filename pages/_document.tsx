bb75d06ee3b1bf26626f9d59b4f2fcadfc96050a
bryanhce <ho.cheng.en.bryan@gmail.com>
2025-01-30 23:57:02 +0800

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,27 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZHXWC7CRX8"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZHXWC7CRX8');
+            `,
+          }}
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
+
       <body>
         <Main />
         <NextScript />
